55b68a851684f6cfd4bca1cc9d0a12719c7c955f
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-16 14:18:55 +0530

    feat: First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pc-ec2-wrapper
+## This is Wrapper Module for EC2 Created by Preet Chauhan
+### For Practice Purpose Only
---
+++ outputs.tf
@@ -0,0 +1,165 @@
+output "id" {
+  description = "The ID of the instance"
+  value       = module.ec2-instance.id
+}
+
+output "arn" {
+  description = "The ARN of the instance"
+  value       = module.ec2-instance.arn
+}
+
+output "capacity_reservation_specification" {
+  description = "Capacity reservation specification of the instance"
+  value       = module.ec2-instance.capacity_reservation_specification
+}
+
+output "instance_state" {
+  description = "The state of the instance"
+  value       = module.ec2-instance.instance_state
+}
+
+output "outpost_arn" {
+  description = "The ARN of the Outpost the instance is assigned to"
+  value       = module.ec2-instance.outpost_arn
+}
+
+output "password_data" {
+  description = "Base-64 encoded encrypted password data for the instance. Useful for getting the administrator password for instances running Microsoft Windows. This attribute is only exported if `get_password_data` is true"
+  value       = module.ec2-instance.password_data
+}
+
+output "primary_network_interface_id" {
+  description = "The ID of the instance's primary network interface"
+  value       = module.ec2-instance.primary_network_interface_id
+}
+
+output "private_dns" {
+  description = "The private DNS name assigned to the instance. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2-instance.private_dns
+}
+
+output "public_dns" {
+  description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2-instance.public_dns
+}
+
+output "public_ip" {
+  description = "The public IP address assigned to the instance, if applicable."
+  value       = module.ec2-instance.public_ip
+}
+
+output "private_ip" {
+  description = "The private IP address assigned to the instance"
+  value       = module.ec2-instance.private_ip
+}
+
+output "ipv6_addresses" {
+  description = "The IPv6 address assigned to the instance, if applicable"
+  value       = module.ec2-instance.ipv6_addresses
+}
+
+output "tags_all" {
+  description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
+  value       = module.ec2-instance.tags_all
+}
+
+output "spot_bid_status" {
+  description = "The current bid status of the Spot Instance Request"
+  value       = module.ec2-instance.spot_bid_status
+}
+
+output "spot_request_state" {
+  description = "The current request state of the Spot Instance Request"
+  value       = module.ec2-instance.spot_request_state
+}
+
+output "spot_instance_id" {
+  description = "The Instance ID (if any) that is currently fulfilling the Spot Instance request"
+  value       = module.ec2-instance.spot_instance_id
+}
+
+output "ami" {
+  description = "AMI ID that was used to create the instance"
+  value       = module.ec2-instance.ami
+}
+
+output "availability_zone" {
+  description = "The availability zone of the created instance"
+  value       = module.ec2-instance.availability_zone
+}
+
+################################################################################
+# EBS Volume(s)
+################################################################################
+
+output "ebs_volumes" {
+  description = "Map of EBS volumes created and their attributes"
+  value       = module.ec2-instance.ebs_volumes
+}
+
+################################################################################
+# IAM Role / Instance Profile
+################################################################################
+
+output "iam_role_name" {
+  description = "The name of the IAM role"
+  value       = module.ec2-instance.iam_role_name
+}
+
+output "iam_role_arn" {
+  description = "The Amazon Resource Name (ARN) specifying the IAM role"
+  value       = module.ec2-instance.iam_role_arn
+}
+
+output "iam_role_unique_id" {
+  description = "Stable and unique string identifying the IAM role"
+  value       = module.ec2-instance.iam_role_unique_id
+}
+
+output "iam_instance_profile_arn" {
+  description = "ARN assigned by AWS to the instance profile"
+  value       = module.ec2-instance.iam_instance_profile_arn
+}
+
+output "iam_instance_profile_id" {
+  description = "Instance profile's ID"
+  value       = module.ec2-instance.iam_instance_profile_id
+}
+
+output "iam_instance_profile_unique" {
+  description = "Stable and unique string identifying the IAM instance profile"
+  value       = module.ec2-instance.iam_instance_profile_unique
+}
+
+################################################################################
+# Block Devices
+################################################################################
+
+output "root_block_device" {
+  description = "Root block device information"
+  value       = module.ec2-instance.root_block_device
+}
+
+output "ebs_block_device" {
+  description = "EBS block device information"
+  value       = module.ec2-instance.ebs_block_device
+}
+
+output "ephemeral_block_device" {
+  description = "Ephemeral block device information"
+  value       = module.ec2-instance.ephemeral_block_device
+}
+
+################################################################################
+# Security Group
+################################################################################
+
+output "security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the security group"
+  value       = module.ec2-instance.security_group_arn
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = module.ec2-instance.security_group_id
+}
